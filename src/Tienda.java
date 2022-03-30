29434dc8638eb3ca50e0a9e89d44cfc119b9c01a
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-30 12:00:42 -0600

    Commit 30/03/2022

---
+++ src/Tienda.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Tienda {
+    //atributos
+    //tipo - string
+    //objetos disponibles - ArrayList<Objetos>
+
+    private String nombre;
+    private ArrayList<Objeto> objetosDisponibles;
+
+
+
+    //metodos
+
+    public boolean usuarioComprar(double dinero, int cantidad, int indiceObjeto){
+        //validar que hay suficientes objetos del tipo requerido (?)
+        //Si hay cantidad y existe en el arreglo
+        //si si tengo, validar que el dinero sea suficiente
+        //si no tengo
+        //hacemos excepcion de que no hay producto y se repite la pregunta
+
+        System.out.println("");
+        return false;
+    }
+    //comprar
+
+    public boolean usuarioVender(Objeto objeto, int cantidad){
+        //logica inversa de comprar
+        //dinero infinito en la tienda
+        //recibir los objetos y aumentar el dinero del jugador
+
+
+        return false;
+    }
+
+
+    //vender
+}
