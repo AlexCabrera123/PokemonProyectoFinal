80a82b9ba420a286dd2bc83dbeadba13f92ea251
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-04-05 11:36:53 -0500

    Commit 05/04/2022

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -10,5 +10,33 @@ public class Entrenador extends Personaje{
         //si su HP es superior a 15
         //escoger 2 fuertes y 1 debil aleatoriamente
         //si no tiene fuertes, poner 3 aleatorios
+     //arreglo de pokemones a usar en la pelea
+    //darle a escoger el pokemon al entrenador
+    //mostrar las estadisticas (HP, velocidad, ataque)
+    //menu para pelar, curar, aumentar ataque o velocidad
+    //si escoge pelear
         //llamar al método pelear del pokemon
+    //si escoge curar
+        //hay que validar si hay pociones/bayas de curacion, usar
+    //si escoge aumentar ataque
+        //hay que validar si hay pociones/bayas de ataque, usar
+    //si escoge velocidad
+        //hay que validar si hay pociones/bayas de velocidad, usar
+
+    //gana --> cuando el entrenador opuesto se quede sin pokemones
+                            //nivel 1 -> 10 XP
+                            //nivel n+1 -> 10 XP * 1.5
+
+    //pierde --> cuando te quedes sin pokemones
+
+    //tirar objeto
+        //mostrar los elementos en la mochila
+        //que el usuario escoja cual tirar
+
+    //intercambiar
+        //entrenador opuesto muestra pokedex
+        //esciji el pokemon que quiero
+        //le propongo uno de la pokedex
+        //y con random true o false acepta
+        //si true
 }
